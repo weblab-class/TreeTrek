e449c0892031a89099e686c8f2867f18186075db
Connie <connieguo3@gmail.com>
2025-01-22 15:40:30 -0500

    deployment

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "mongodb+srv://connieh:timtamGreen@treetrek.d4u0q.mongodb.net/?retryWrites=true&w=majority&appName=TreeTrek";
+const mongoConnectionURL = process.env.MONGO_SRV;
 // TODO change database name to the name you chose
 const databaseName = "TreeTrek";
 
@@ -64,7 +64,7 @@ app.use(express.json());
 app.use(
   session({
     // TODO: add a SESSION_SECRET string in your .env file, and replace the secret with process.env.SESSION_SECRET
-    secret: "session-secret",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
