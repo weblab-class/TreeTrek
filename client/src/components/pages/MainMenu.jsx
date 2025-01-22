c36d0bcf99c2735e4d1a099e3784b9287ad00927
Connie <connieguo3@gmail.com>
2025-01-22 16:16:37 -0500

    some css changes

--- client/src/components/pages/MainMenu.jsx
+++ client/src/components/pages/MainMenu.jsx
@@ -6,17 +6,17 @@ import './MainMenu.css';
 
 const MainMenu = () => {
     let navigate = useNavigate(); 
-    const routeChange = () =>{ 
-        navigate("/game");
+    const routeChange = () =>{   //on singleplayer click, redirect to game page
+        navigate("/game");  
     }
         return (
         <div className = "main-menu">
             <h1>Main Menu </h1>
             <div className = "menu-options">
                 <button onClick={routeChange}>
-                    <img src="/singleplayerButton.png" alt="SinglePlayer" style={{width:"150px", height:"auto"}}/>
+                    <img src="/singleplayerButton.png" alt="SinglePlayer" style={{width:"205px", height:"auto"}}/>
                 </button>
-                {/* <button> Multiplayer </button>
+                {/* <button> Multiplayer </button> ##to be implemented later##
                 <button> Settings </button>
                 <button> High Scores </button> */}
             </div>
