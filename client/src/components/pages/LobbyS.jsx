afbc6de5b7426c54246fa961a5f04c4e505ab1b6
Connie <connieguo3@gmail.com>
2025-01-24 00:42:28 -0500

    LobbyS UI Beginnings

--- client/src/components/pages/LobbyS.jsx
+++ client/src/components/pages/LobbyS.jsx
@@ -1,23 +1,58 @@
-import React from 'react'
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from '../App';
 
 import "../../utilities.css";
 import './LobbyS.css';
 
 const LobbyS = () => {
+    const { userId } = useContext(UserContext);
+    console.log(userId);
     let navigate = useNavigate();
 
+    let spawnButton = null;
+    if (userId) {
+        spawnButton = (
+            <div>
+                <button className="Game-spawn"
+                    onClick={() => {
+                    post("/api/spawn", { userid: userId });
+                    navigate("/game");
+                    }}
+                >
+                    Play!
+                </button>
+            </div>
+        );
+    }; 
+
     return (
-            <div className = "MainMenu">
-                <h1>Main Menu</h1>
-                <div className = "MainMenu-options">
+
+
+
+            <div className = "character-selection">
+                <h1>Character Selection</h1>
+                <div className = "message-box">
+
+                </div>
+                <div className = "character-selection-options">
+                    <div className = "character">
+
+                    </div>
+                    <button className = "left-button"> 
+
+                    </button>
+                    <button className = "right-button">
+
+
+                    </button>
+                </div>
+                {/* <div className = "MainMenu-options">
                     <button onClick={() => navigate("/game")}>
                         <img src="../singleplayerButton.png" alt="SinglePlayer" style={{width:"205px", height:"auto"}}/>
                     </button>
-                    {/* <button> Multiplayer </button> ##to be implemented later##
-                    <button> Settings </button>
-                    <button> High Scores </button> */}
-                </div>
+                </div> */}
+                {spawnButton}
             </div>
     );
 };
